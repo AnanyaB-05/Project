10876f0dd43b7f27920720817bdac1f9ca1c7044
Ananya-05 <ananyabolla02@gmail.com>
2026-03-04 13:23:41 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 20;
+
+        // Multiplying two numbers using * operator
+        int product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+
